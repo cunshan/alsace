564f7b1f6af546797727aa41ecf23a9de96bfe18
王存山 <wangcunshan62@camelotchina.com>
2019-09-24 11:12:00 +0800

    init project

---
+++ alsace-web/src/main/java/com/alsace/web/AlsaceWebApplication.java
@@ -0,0 +1,13 @@
+package com.alsace.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlsaceWebApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AlsaceWebApplication.class);
+  }
+
+}
