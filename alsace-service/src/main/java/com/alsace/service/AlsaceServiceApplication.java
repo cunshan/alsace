ac71f2d7d5c4ded6bc6f20766aa0d89a3a40711e
王存山 <wangcunshan62@camelotchina.com>
2019-09-25 11:03:53 +0800

    数据库相关 jpa  mybatis  druid 集成

---
+++ alsace-service/src/main/java/com/alsace/service/AlsaceServiceApplication.java
@@ -0,0 +1,15 @@
+package com.alsace.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.alsace.service")
+public class AlsaceServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AlsaceServiceApplication.class);
+  }
+
+}
