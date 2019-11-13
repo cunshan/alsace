9d8c7bce0b4cecb67471bce1d5dbd728a3f79239
王存山 <wangcunshan62@camelotchina.com>
2019-11-13 14:36:32 +0800

    修改example工程

--- alsace-service/src/main/java/com/example/service/user/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.service.user.repository;
-
-import com.example.service.user.domain.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-}
--- alsace-service/src/main/java/com/example/service/user/serivce/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.service.user.serivce;
-
-import com.alsace.framework.common.basic.BaseService;
-import com.example.service.user.domain.User;
-
-public interface UserService extends BaseService<User,Long> {
-
-}
