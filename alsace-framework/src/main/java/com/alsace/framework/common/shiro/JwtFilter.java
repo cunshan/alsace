9b864f9c49e2b61c3920caaf5de5d1f37bca22e5
王存山 <wangcunshan62@camelotchina.com>
2020-02-18 23:05:52 +0800

    调整JWT配置

--- alsace-framework/src/main/java/com/alsace/framework/common/shiro/JwtFilter.java
+++ alsace-framework/src/main/java/com/alsace/framework/common/shiro/JwtFilter.java
@@ -3,36 +3,27 @@ package com.alsace.framework.common.shiro;
 import com.alsace.framework.common.basic.AlsaceResponse.Builder;
 import com.alsace.framework.utils.GsonUtils;
 import com.alsace.framework.utils.LogUtils;
+import java.io.IOException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.web.filter.authc.BasicHttpAuthenticationFilter;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Slf4j
 public class JwtFilter extends BasicHttpAuthenticationFilter {
 
 
-  @Override
-  protected boolean isAccessAllowed(ServletRequest request, ServletResponse response,
-      Object mappedValue) {
+  private static final String BEARER = "Bearer";
 
-    boolean res;
-    try {
-      res = executeLogin(request, response);
-    } catch (Exception e) {
-      LogUtils.error(log, e);
-      return false;
-    }
-
-    return res;
+  public JwtFilter() {
+    setAuthcScheme(BEARER);
+    setAuthzScheme(BEARER);
   }
-
 
   /**
    * 跨域支持
@@ -46,31 +37,54 @@ public class JwtFilter extends BasicHttpAuthenticationFilter {
     httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE");
     httpServletResponse.setHeader("Access-Control-Allow-Headers",
         httpServletRequest.getHeader("Access-Control-Request-Headers"));
-    // 跨域时会首先发送一个option请求，这里我们给option请求直接返回正常状态
-    if (httpServletRequest.getMethod().equals(RequestMethod.OPTIONS.name())) {
-      httpServletResponse.setStatus(HttpStatus.OK.value());
-      return false;
-    }
+//    // 跨域时会首先发送一个option请求，这里我们给option请求直接返回正常状态
+//    if (httpServletRequest.getMethod().equals(RequestMethod.OPTIONS.name())) {
+//      httpServletResponse.setStatus(HttpStatus.OK.value());
+//      return false;
+//    }
+    //设置返回头信息
+    response.setCharacterEncoding("UTF-8");
+    response.setContentType("application/json;charset=UTF-8");
     return super.preHandle(request, response);
   }
 
-  @Override
-  protected boolean executeLogin(ServletRequest request, ServletResponse response)
-      throws Exception {
-    HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+  /**
+   * 获取头部token
+   */
+  private JwtToken getJwtToken(ServletRequest request) {
     //获取头部token
-    String header = httpServletRequest.getHeader(HttpHeaders.AUTHORIZATION);
-    String token = header.substring(header.indexOf(' ')+1);
-    JwtToken jwtToken = new JwtToken(token);
-    //登录操作
+    String header = this.getAuthzHeader(request);
+    String token = header.substring(header.indexOf(' ') + 1);
+    return new JwtToken(token);
+  }
+
+
+  @Override
+  protected AuthenticationToken createToken(ServletRequest request, ServletResponse response) {
+    return getJwtToken(request);
+  }
+
+
+  @Override
+  protected boolean onLoginFailure(AuthenticationToken token, AuthenticationException ex,
+      ServletRequest request, ServletResponse response) {
     try {
-      getSubject(request, response).login(jwtToken);
-    }catch (AuthenticationException ex){
-      response.setCharacterEncoding("UTF-8");
-      response.setContentType("application/json;charset=UTF-8");
-      response.getWriter().println(GsonUtils.toJson(new Builder(false).code(HttpStatus.UNAUTHORIZED.value()).msg(ex.getMessage()).build()));
-      return false;
+      response.getWriter().println(GsonUtils.toJson(
+          new Builder(false).code(HttpStatus.UNAUTHORIZED.value()).msg(ex.getMessage()).build()));
+    } catch (IOException e) {
+      LogUtils.error(log, e);
     }
-    return true;
+    return false;
+  }
+
+  @Override
+  protected boolean sendChallenge(ServletRequest request, ServletResponse response) {
+    try {
+      response.getWriter().println(GsonUtils.toJson(
+          new Builder(false).code(HttpStatus.UNAUTHORIZED.value()).msg("Token无效，请重新登录！").build()));
+    } catch (IOException e) {
+      LogUtils.error(log, e);
+    }
+    return false;
   }
 }
