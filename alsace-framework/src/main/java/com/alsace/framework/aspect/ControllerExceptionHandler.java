09217108889e61dd8d35fb136be80534fc62e6de
王存山 <wangcunshan62@camelotchina.com>
2020-02-19 16:11:13 +0800

    统一异常处理

--- alsace-framework/src/main/java/com/alsace/framework/aspect/ControllerExceptionHandler.java
+++ alsace-framework/src/main/java/com/alsace/framework/aspect/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.alsace.framework.aspect;
 
 import com.alsace.framework.common.basic.AlsaceResponse;
+import com.alsace.framework.common.exception.BizException;
 import com.alsace.framework.utils.LogUtils;
 import com.google.common.base.Throwables;
 import lombok.extern.slf4j.Slf4j;
@@ -21,9 +22,32 @@ public class ControllerExceptionHandler {
   @ExceptionHandler(AuthenticationException.class)
   @ResponseBody
   public AlsaceResponse authenticationException(AuthenticationException ex){
-    LogUtils.info(log, Throwables.getStackTraceAsString(ex));
+    LogUtils.error(log, Throwables.getStackTraceAsString(ex));
     return new AlsaceResponse.Builder(false).code(HttpStatus.UNAUTHORIZED.value()).msg(ex.getMessage()).build();
   }
+
+  /**
+   * 业务异常处理
+   */
+  @ExceptionHandler(BizException.class)
+  @ResponseBody
+  public AlsaceResponse bizException(BizException ex){
+    LogUtils.error(log, Throwables.getStackTraceAsString(ex));
+    return new AlsaceResponse.Builder(false).code(HttpStatus.OK.value()).msg(ex.getMessage()).build();
+  }
+
+
+  /**
+   * 未处理的异常处理
+   */
+  @ExceptionHandler(Exception.class)
+  @ResponseBody
+  public AlsaceResponse bizException(Exception ex){
+    LogUtils.error(log, Throwables.getStackTraceAsString(ex));
+    return new AlsaceResponse.Builder(false).code(HttpStatus.OK.value()).msg(ex.getMessage()).build();
+  }
+
+
 
 
 }
