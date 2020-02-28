6adaeb841a7edb13d8073f3bf0fa88fe78683bc2
王存山 <wangcunshan62@camelotchina.com>
2020-02-28 13:02:09 +0800

    分页优化

--- alsace-framework/src/main/java/com/alsace/framework/common/basic/PageQueryService.java
+++ alsace-framework/src/main/java/com/alsace/framework/common/basic/PageQueryService.java
@@ -13,4 +13,12 @@ public class PageQueryService {
   protected <P extends BasePageParam> void startPageQuery(P param,boolean countSql){
     PageHelper.startPage(param.getPageNum(),param.getPageSize(),countSql);
   }
+
+  protected <P extends BasePageParam> void startPageQuery(P param,String orderBy){
+    PageHelper.startPage(param.getPageNum(),param.getPageSize(),orderBy);
+  }
+
+  protected <P extends BasePageParam> void startPageQuery(P param,String orderBy,boolean countSql){
+    PageHelper.startPage(param.getPageNum(),param.getPageSize(),countSql).setOrderBy(orderBy);
+  }
 }
